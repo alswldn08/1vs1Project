22535b0c9517f8486c1bc2010e7b6633a7c0f874
민지우 <jiwoojei778899@gmail.com>
2025-04-29 20:11:15 +0900

    fix rigid and colider

---
+++ Assets/Scripts/Player/PlayerEnergy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerEnergy : MonoBehaviour
+{
+    public Slider energy;
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
